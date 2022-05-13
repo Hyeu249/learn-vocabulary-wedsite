e33bf001ed978d8d1eb50202d12573bfb04d3755
Bao Hieu <labaohieu@gmail.com>
2022-05-13 18:27:58 +0700

    new vocabulary

---
+++ src/public/vocabularyJson/noun_phrase.js
@@ -0,0 +1,25 @@
+import ANSWER_KEY from "./test1.js";
+
+const json = [
+  // "____________________PHRASAL_NOUNS____________________",
+  "recruit orientation (n): định hướng tuyển dụng",
+  "sheet of card stock (n): tấm bìa cứng(card stock === bìa cứng)",
+  "on short notice (p): trong thời gian ngắn",
+  "the resort guest (n): du khách nghỉ dưỡng",
+  "item code (n): mã sản phẩm",
+  "light-emitting (n): phát quang",
+  "competitive attribute (n): lợi thế cạnh tranh(thuộc tính cạnh tranh)",
+  "bright light (n): sáng rực rỡ",
+  "a less-than-ideal (n): kém lý tưởng",
+  "precautionary measure (n): biện pháp phòng ngừa",
+  "the company's occupational safety department (n): bộ phận an toàn lao động của công ty",
+  // "____________________PHRASAL_VERBS____________________",
+  "lean toward (v): nghiêng về",
+];
+
+export { json as namE };
+// export { ANSWER_KEY as namE };
+
+//Create vocabulary convention
+//nếu 1 từ, đặt bình thường
+//nếu 2 từ, thêm dấu chấm cuối câu(nếu có dấu đóng ngoặc, cũng thêm dấu chấm sau nó)
