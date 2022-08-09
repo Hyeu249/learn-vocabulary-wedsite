576268e0250f23b25104d8cc081603c5b8b9b1f7
Bao Hieu <labaohieu@gmail.com>
2022-08-09 16:30:34 +0700

    update

---
+++ src/public/vocabularyJson/vocabulary_for_coder.js
@@ -0,0 +1,58 @@
+const json = [
+  //   "aggregate (n): tổng hợp, toàn bộ",
+  //   "syllabus (n): giáo trình",
+  //   "yield (v): mang lại",
+  //   "radical (adj): triệt để",
+  //   "synthesize (v): tổng hợp",
+  //   "jargon (n): biệt ngữ",
+  //   "deviate (v): đi lệch, trệch hướng",
+  //   "replicate (v): sao chép",
+  //   "subset (n): tập hợp con",
+  //   "whittle (v): gọt",
+  //   "caveat (n): báo trước",
+  //   "laborious (adj): tốn công sức",
+  //   "cliffhanger (n): câu chuyện hấp dẫn",
+  //   "dilemma (n): tình huống khó xử",
+  //   "shelter (n): trú ẩn(mái che, chòi, phòng)",
+  //   "redundant (n): dư thừa",
+  //   "acronym (n): từ viết tắt",
+  //   "arsenal (n): kho vũ khí",
+  //   "alias (n): bí danh",
+  //   "slaughter (v): tàn sát",
+  //   "pluralize (n): đa dạng hóa",
+  //   "extrapolate (v): ngoại suy",
+  //   "arbitrary (n): bất kỳ",
+  //   "onslaught (n): sự tấn công dữ dội, phẫn nộ dữ dội",
+  //   "hatred (N): căm thù",
+  //   "refine (v): lọc, tinh chỉnh",
+  //   "conjunction (n): kết hợp",
+  //   "onwards (adverb): trở đi",
+  //   "odd (adj): kỳ quặc",
+  //   "harm (n): thiệt hại",
+  //   "payoff (n): phần thưởng(thưởng)",
+  //   "encompass (v): toàn bộ, tổng thể",
+  //   "camouflage (n): ngụy trang, quân phục dã ngoại",
+  //   "bog (v,n): sa lầy, vũng lầy",
+  //   "constraint (n): hạn chế, ràng buộc",
+  //   "clunky (adj): nặng nề, bất tiện",
+  //   "ramble (v): lan man",
+  //   "superstitious (adj): mê tín",
+  //   "crux (n): mấu chốt",
+  //   "ambiguity (n): sự mơ hồ",
+  //   "incline (n): nghiêng về, có khuynh hướng(mặt nghiêng, con dốc)",
+  //   "synthesis (n): tổng hợp",
+  //   "monstrosity (n): quái dị",
+  //   "deprecate (v): ngừng, không dùng nữa",
+  //   "instantiate (v): tạo",
+  //   "irreducible (adj): không thể điều khiển được",
+  //   "reminisce (v): hồi tưởng",
+];
+
+export default json;
+
+//Create vocabulary convention
+//nếu 1 từ, đặt bình thường
+//nếu 2 từ, thêm dấu chấm cuối câu(nếu có dấu đóng ngoặc, cũng thêm dấu chấm sau nó)
+
+//note ngữ pháp
+//1.be more ... ? 100% '...' = tính từ
